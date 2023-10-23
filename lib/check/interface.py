c5bee41712608afa897e2af0120dcd536cfc2d13
unknown <koos@cesbit.com>
2023-10-23 13:31:05 +0200

    #4

--- lib/check/interface.py
+++ lib/check/interface.py
@@ -1,6 +1,7 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from collections import Counter
 from libprobe.asset import Asset
+from libprobe.exceptions import CheckException
 from ..snmpquery import snmpquery
 
 QUERIES = (
@@ -54,6 +55,87 @@ _64_BIT_COUNTERS = (
     'HCOutBroadcastPkts',
 )
 
+ExcludedIfTypes = (
+    'ieee80211',
+    'l3ipvlan',
+    'l2vlan',
+)
+
+ExcludedIfDescStartsWith = (
+    'veth',
+    'lo',
+    'nu',
+    'vnet',
+    'virbr',
+)
+
+ExcludedIfDescContains = (
+    'vif',
+    'stackport',
+    'internal-data',
+    'cplane'
+)
+
+# Address and prefixes matching these prefixes will be absolutely filtered
+ReservedAddresses = (
+    '00:00:01:00:00:01',    # Problematic XEROX CORPORATION MACs
+    '00:00:01',             # Cisco ASA virtual MACs
+    '01:00:01',
+    '00:01:00:00:00:01',
+    '00:08:e3',             # Cisco unicast
+    '00:21:00:00:00:22',
+    '00:13:00:00:00:14',
+    '00:0f:b7:48:48',
+    '00:16:00:00:00:16',
+    '00:00:15:00:00:00',
+    '00:15:00:00:00:15',
+    '00:14:00:00:00:14',
+    '00:14:00:00:00:15',
+    '00:01:00:00:00:01',
+    '00:21:00:00:00:21',
+    '00:21:00:00:00:22',
+    '7a:77:00:00:00:0',
+    '00:00:03:00:00:00',
+    '00:00:05:00:00:00',
+    '00:08:e3:ff:fc:28',   # Problematic Cisco MACs
+    '00:08:e3:ff:fd:90',
+    '02:00:4c:4f:4f:50',
+    '02:50:f2:00:00:01',
+    '00:25:b5:00:00:0f',
+    '00:25:b5:00:00:1f',
+    '00:18:18:16',
+    '12:00:00:00:00:00',
+    '54:10:ec',            # Microchip
+    '00:90:fa',            # Emulex
+    'cc:4e:24',            # PCS
+    '00:90:8f',            # Audio codes
+    '38:90:a5:be',         # Threat Defense
+    'b4:0c:25:e',          # Palo Alto Firewall HA
+    '00:1b:17:00',
+    'ba:db:ad',            # Palo Alto VMware interface
+    '00:a0:c9:00:00:00',   # Firepower
+    '00:13:00:00:00:13',   # Problematic LLDP chassis ID
+    '00:00:00',            # ARP
+    '01:00:00:00:00',
+    '01:00:5e',            # Used for IPV4 Multicast and MLPS Multicast
+    '33:33',               # Reserved for IPV6 Multicast
+    '02:00:4c:4f:4f:50',   # Microsoft Loopback adapter`
+    '20:41:53:59:4e:ff',   # RAS
+    '00:22:bd:f8:19:ff',   # Cisco ACI
+    '00:0b:ca:fe:00:00',   # Avaya/Xen
+    '02:00:00',            # Common default
+    '02:00:01',
+    '00:00:03',            # Problematic Cisco ASA mac
+    '00:07:b4:00',         # GLBP
+    '00:09:0f:09',         # Fortinet HA
+    '00:10:db:ff:10',      # Internal interfaces for Juniper
+    '02:42:ac:11',         # Docker
+    '00:ff:c2:f3:cb:94',   # Windows
+    '1e:8e:39:50:50:05:05:69:00',  # VMware PVSCSI Controller
+    'c8:4f:86:fc:00',      # Sophos virutal HA MAC
+    '02:0f:00:0b:98',      # Sophos virutal HA MAC
+)
+
 
 async def check_interface(
         asset: Asset,
@@ -67,7 +149,20 @@ async def check_interface(
     if_x_entry = {i.pop('name'): i for i in state_data.pop('ifX', [])}
     for item in itms:
         key = item['name']
-        name = item['Descr']
+        if item.get('Type') in ExcludedIfTypes:
+            continue
+        mac = item.get('PhysAddress')
+        if isinstance(mac, str) and any(
+                mac.startswith(e) for e in ReservedAddresses):
+            continue
+        try:
+            name = item['Descr']
+        except KeyError:
+            raise CheckException('Missing required ifDesc OID for interface')
+        if isinstance(name, str) and (
+            any(name.startswith(e) for e in ExcludedIfDescStartsWith) or
+                any(e in name for e in ExcludedIfDescContains)):
+            continue
         idx = counts[name]
         counts[name] += 1
         item['name'] = f'{name}_{idx}' if idx else name
