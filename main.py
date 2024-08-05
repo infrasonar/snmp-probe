de0deb9e650ab32a5c06f819e1ad1afb3c47a8d1
Koos85 <koosjoosten@gmail.com>
2024-08-05 10:04:53 +0200

    added 3 more checks.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib.check.base import check_base
 from lib.check.device import check_device
 from lib.check.entity import check_entity
 from lib.check.interface import check_interface
+from lib.check.ip import check_ip
 from lib.check.ip_address import check_ip_address
 from lib.check.ip_forward import check_ip_forward
 from lib.check.power_ethernet import check_power_ethernet
@@ -11,9 +12,11 @@ from lib.check.processor import check_processor
 from lib.check.sensor import check_sensor
 from lib.check.storage import check_storage
 from lib.check.system import check_system
+from lib.check.tcp import check_tcp
 from lib.check.tcp_connection import check_tcp_connection
 from lib.check.tcp_listener import check_tcp_listener
 from lib.check.ucd import check_ucd
+from lib.check.udp import check_udp
 from lib.version import __version__ as version
 
 
@@ -23,6 +26,7 @@ if __name__ == '__main__':
         'device': check_device,
         'entity': check_entity,
         'interface': check_interface,
+        'ip': check_ip,
         'ipAddress': check_ip_address,
         'ipForward': check_ip_forward,
         'powerEthernet': check_power_ethernet,
@@ -31,9 +35,11 @@ if __name__ == '__main__':
         'sensor': check_sensor,
         'storage': check_storage,
         'system': check_system,
+        'tcp': check_tcp,
         'tcpConnection': check_tcp_connection,
         'tcpListener': check_tcp_listener,
         'ucd': check_ucd,
+        'udp': check_udp,
     }
 
     probe = Probe("snmp", version, checks)
