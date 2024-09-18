d7ccc2f98af208861949a5d004b12b87047a48f5
Koos85 <koosjoosten@gmail.com>
2024-09-18 13:56:43 +0200

    python3.12. check config like in asyncsnmplib. fix exception for 3 new checks

--- lib/snmpquery.py
+++ lib/snmpquery.py
@@ -1,128 +1,88 @@
 import logging
 from asyncsnmplib.client import Snmp, SnmpV1, SnmpV3
-from asyncsnmplib.exceptions import SnmpException
 from asyncsnmplib.exceptions import SnmpNoAuthParams
 from asyncsnmplib.exceptions import SnmpNoConnection
 from asyncsnmplib.mib.utils import on_result
 from asyncsnmplib.v3.auth import AUTH_PROTO
 from asyncsnmplib.v3.encr import PRIV_PROTO
 from libprobe.asset import Asset
+from libprobe.exceptions import CheckException
+from typing import Tuple, Dict, List, Any
+from . import DOCS_URL
 
 
-class InvalidCredentialsException(SnmpException):
-    message = 'Invalid SNMP v3 credentials.'
-
-
-class InvalidClientConfigException(SnmpException):
-    message = 'Invalid SNMP v3 client configuration.'
-
-
-class InvalidSnmpVersionException(SnmpException):
-    message = 'Invalid SNMP version.'
-
-
-class ParseResultException(SnmpException):
-    def __init__(self, message: str):
-        super().__init__(message)
-        self.message = message
-
-
-def snmpv3_credentials(asset_config: dict):
-    try:
-        user_name = asset_config['username']
-    except KeyError:
-        raise Exception(f'missing `username`')
-
-    auth = asset_config.get('auth')
-    if auth is not None:
-        auth_type = auth.get('type', 'USM_AUTH_NONE')
-        if auth_type != 'USM_AUTH_NONE':
-            if auth_type not in AUTH_PROTO:
-                raise Exception(f'invalid `auth.type`')
-
-            try:
-                auth_passwd = auth['password']
-            except KeyError:
-                raise Exception(f'missing `auth.password`')
-
-            priv = asset_config.get('priv', {})
-            priv_type = priv.get('type', 'USM_PRIV_NONE')
-            if priv_type != 'USM_PRIV_NONE':
-                if priv_type not in PRIV_PROTO:
-                    raise Exception(f'invalid `priv.type`')
-
-                try:
-                    priv_passwd = priv['password']
-                except KeyError:
-                    raise Exception(f'missing `priv.password`')
-
-                return {
-                    'username': user_name,
-                    'auth_proto': auth_type,
-                    'auth_passwd': auth_passwd,
-                    'priv_proto': priv_type,
-                    'priv_passwd': priv_passwd,
-                }
-            else:
-                return {
-                    'username': user_name,
-                    'auth_proto': auth_type,
-                    'auth_passwd': auth_passwd,
-                }
-        else:
-            return {
-                'username': user_name,
-            }
+class SnmpInvalidConfig(Exception):
+    pass
 
 
 async def snmpquery(
         asset: Asset,
         asset_config: dict,
         check_config: dict,
-        queries: dict):
+        oids: Tuple[Tuple[int], ...]) -> Dict[str, List[Dict[str, Any]]]:
     address = check_config.get('address')
     if address is None:
         address = asset.name
 
     version = asset_config.get('version', '2c')
 
-    if version == '2c':
-        community = asset_config.get('community', 'public')
-        if isinstance(community, dict):
-            community = community.get('secret')
-        if not isinstance(community, str):
-            raise TypeError('SNMP community must be a string.')
-        cl = Snmp(
-            host=address,
-            community=community,
-        )
-    elif version == '3':
-        try:
-            cred = snmpv3_credentials(asset_config)
-        except Exception as e:
-            logging.warning(f'invalid snmpv3 credentials {asset}: {e}')
-            raise InvalidCredentialsException
-        try:
+    try:
+        if version == '2c':
+            community = asset_config.get('community', 'public')
+            if isinstance(community, dict):
+                community = community.get('secret')
+            if not isinstance(community, str):
+                raise SnmpInvalidConfig('`community` must be a string.')
+            cl = Snmp(
+                host=address,
+                community=community,
+            )
+        elif version == '3':
+            username = asset_config.get('username')
+            if not isinstance(username, str):
+                raise SnmpInvalidConfig('`username` must be a string.')
+            auth = asset_config.get('auth')
+            if auth:
+                auth_proto = AUTH_PROTO.get(auth.get('type'))
+                auth_passwd = auth.get('password')
+                if auth_proto is None:
+                    raise SnmpInvalidConfig('`auth.type` invalid')
+                elif not isinstance(auth_passwd, str):
+                    raise SnmpInvalidConfig('`auth.password` must be string')
+                auth = (auth_proto, auth_passwd)
+            priv = auth and asset_config.get('priv')
+            if priv:
+                priv_proto = PRIV_PROTO.get(priv.get('type'))
+                priv_passwd = priv.get('password')
+                if priv_proto is None:
+                    raise SnmpInvalidConfig('`priv.type` invalid')
+                elif not isinstance(priv_passwd, str):
+                    raise SnmpInvalidConfig('`priv.password` must be string')
+                priv = (priv, priv_passwd)
             cl = SnmpV3(
                 host=address,
-                **cred,
+                username=username,
+                auth=auth,
+                priv=priv,
             )
-        except Exception as e:
-            logging.warning(f'invalid snmpv3 client config {asset}: {e}')
-            raise InvalidClientConfigException
-    elif version == '1':
-        community = asset_config.get('community', 'public')
-        if isinstance(community, dict):
-            community = community.get('secret')
-        if not isinstance(community, str):
-            raise TypeError('SNMP community must be a string.')
-        cl = SnmpV1(
-            host=address,
-            community=community,
-        )
-    else:
-        logging.warning(f'unsupported snmp version {asset}: {version}')
-        raise InvalidSnmpVersionException
+        elif version == '1':
+            community = asset_config.get('community', 'public')
+            if isinstance(community, dict):
+                community = community.get('secret')
+            if not isinstance(community, str):
+                raise SnmpInvalidConfig('`community` must be a string.')
+            cl = SnmpV1(
+                host=address,
+                community=community,
+            )
+        else:
+            raise SnmpInvalidConfig(f'unsupported snmp version {version}')
+    except SnmpInvalidConfig as e:
+        msg = str(e) or type(e).__name__
+        logging.error(f'Invalid config. Exception: {msg}')
+        raise CheckException(
+            'Invalid config. Please refer to the following documentation'
+            f' for detailed instructions: <{DOCS_URL}>')
 
     try:
         await cl.connect()
@@ -134,13 +94,13 @@ async def snmpquery(
     else:
         results = {}
         try:
-            for oid in queries:
+            for oid in oids:
                 result = await cl.walk(oid)
                 try:
                     name, result = on_result(oid, result)
                 except Exception as e:
                     msg = str(e) or type(e).__name__
-                    raise ParseResultException(
+                    raise CheckException(
                         f'Failed to parse result. Exception: {msg}'
                     )
                 else:
