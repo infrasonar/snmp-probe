93fa7adb97afda9d27e06df932d7720441a3a218
Koos85 <koosjoosten@gmail.com>
2025-06-20 15:40:31 +0200

    raise nice checkerror for single item types with no metrics

--- lib/check/tcp.py
+++ lib/check/tcp.py
@@ -1,5 +1,6 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from libprobe.asset import Asset
+from libprobe.exceptions import CheckException
 from ..snmpclient import get_snmp_client
 from ..snmpquery import snmpquery
 
@@ -25,5 +26,18 @@ async def check_tcp(
             if _64_bit_name in item:
                 _32_bit_name = _64_bit_name[2:]
                 item[_32_bit_name] = item.pop(_64_bit_name)
+
+    try:
+        items = state['tcp']
+        assert len(items)
+        # There is an item with a 'name' metric in case the root oid can be
+        # found, and only nested oids are present.
+        # We filter out (is_table=False) these.
+        item = items[0]
+        assert len(item) > 1
+    except Exception:
+        raise CheckException(
+            'SNMP is connected, but this device does not provide TCP '
+            'information. You may want to disable this check.')
 
     return state
