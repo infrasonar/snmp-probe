0dee6d3bc187c078b09a8c3d2d24e2e8eeed2035
Koos85 <koosjoosten@gmail.com>
2025-06-20 15:54:40 +0200

    revert change udp check

--- lib/check/udp.py
+++ lib/check/udp.py
@@ -1,6 +1,5 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from libprobe.asset import Asset
-from libprobe.exceptions import CheckException
 from ..snmpclient import get_snmp_client
 from ..snmpquery import snmpquery
 
@@ -26,18 +25,5 @@ async def check_udp(
             if _64_bit_name in item:
                 _32_bit_name = _64_bit_name[2:]
                 item[_32_bit_name] = item.pop(_64_bit_name)
-
-    try:
-        items = state['udp']
-        assert len(items)
-        # There is an item with a 'name' metric in case the root oid can be
-        # found, and only nested oids are present.
-        # We filter out (is_table=False) these.
-        item = items[0]
-        assert len(item) > 1
-    except Exception:
-        raise CheckException(
-            'SNMP is connected, but this device does not provide general UDP '
-            'information. You may want to disable this check.')
 
     return state
